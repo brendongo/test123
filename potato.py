ce80e2537d89d1e2a066e145bca0cac6ecaa85d1
brendon <brendon.go@gmail.com>
2021-02-03 15:37:07 -0800

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Fail eqeq

--- potato.py
+++ potato.py
@@ -2,17 +2,9 @@ x = 1
 if x is x:
 	print("hi")
 
-if y == y:
+if a == a:
 	print("useless")
 
 if x is "potato":
 	print("hello")
 
-a = 123456789
-5 == 5
-"hi" is "hi"
-print('hello')
-client("s3", aws_secret_access_key="jWnyxxxxxxxxxxxxxxxxX7ZQxxxxxxxxxxxxxxxx")
-
-if z == z:
-	print("blaasdlfasdf")
