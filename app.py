8bbb443ed94cc950f842357b58b7eeed274cb4f6
brendon <brendon.go@gmail.com>
2021-02-09 13:02:19 -0800

    fixup! fixup! fixup! Rename

--- app.py
+++ app.py
@@ -167,5 +167,5 @@ import requests
 
 def get_objects(obj_id):
 	url = f"https://api.service.com/objects/{obj_id}"
-	response = requests.get(url)
-	return response
+	resp = requests.get(url)
+	return resp
