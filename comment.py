28153b6f18c808972b685ccf2cd0837af2504e95
brendon <brendon.go@gmail.com>
2021-05-14 16:14:18 -0700

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Testing gha if

--- comment.py
+++ comment.py
@@ -2,8 +2,16 @@ import requests
 import sys
 
 def main():
-	if argv[1] == "strict":
-		assert False
+    session = requests.Session()
+    session.headers["Authorization"] = f"token {sys.argv[1]}"
+    url = f"https://api/github.com/repos/returntocorp/semgrep/pulls/{sys.argv[2]}/comments"
+    message = "Test message"
 
+    pr_comment_payload = {
+        "body": message,
+        "commit_id": sys.argv[3]
+    }
+
+    session.post(url,json=pr_comment_payload)
 
 main()
